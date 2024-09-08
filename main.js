e82e3db37f205e6cd7b3ec845bff54c3f38a65d2
Ha Duc Kien <haduckien1709@gmail.com>
2024-09-08 10:58:45 +0700

    second

---
+++ main.js
@@ -0,0 +1,20 @@
+const imgaesale = document.getElementById('image-sale');
+const imgcom1 = document.getElementById('image-combo1');
+const imgcom2 = document.getElementById('image-combo2');
+const imgcom3 = document.getElementById('image-combo3');
+const imgcom4 = document.getElementById('image-combo4');
+const imgcom5 = document.getElementById('image-combo5');
+const imgcom6 = document.getElementById('image-combo6');
+const imgcom7 = document.getElementById('image-combo7');
+const imggroup2 = document.getElementById('image-group2');
+const imggroup2pp = document.getElementById('image-group2++');
+const imggroup3 = document.getElementById('image-group3');
+const imggroup4 = document.getElementById('image-group4');
+const imggroup5 = document.getElementById('image-group5');
+const imggroup6 = document.getElementById('image-group6');
+imgaesale.addEventListener('click', () => {
+    window.location.href = "https://haduckientechnology17092005.github.io/combo2/";
+})
+imggroup2.addEventListener('click', () => {
+    window.location.href = "https://haduckientechnology17092005.github.io/combo2/";
+})
